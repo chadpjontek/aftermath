82907445fa0bc1cd66a3e51ccbe84fd63dd48719
Chad Pjontek <chadpjontek@gmail.com>
2020-02-24 15:35:23 -0500

    fix: Correct image size of Feb 23rd news

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,22 +11,20 @@ const Home = () => {
           <img src={logo} className="image" alt="aftermath logo" />
           <p className="h1-desc">Horde - Benediction - PvP - US</p>
         </div>
-        <div className="container">
-          <p className='news-date'>Feb 23rd, 2020</p>
-        </div >
-        <img className='news-img' src={nef} alt='Screenshot of Aftermath posing in front of the corpse of Nefarian' />
-        <p className='news-p'>
-          The Lord of Blackrock Spire and son of Deathwing has been defeated. Epic cheers arose as his still body lay smoldering at our feet. &lt;Insert more RP text here&gt;
-      </p>
-        <p className='news-p'>
-          Great job everyone! Lot's of hard work put in with many consumes used! Got some great drops these first two weeks and looking forward to many more before Zul'Gurub!
-      </p>
-        <p className='news-p'>
-          We are looking for a few more good players to join our ranks (shaman and warlocks in particular.) Join our discord to find out more!
-      </p>
-        <div className='empty'></div>
-        <p className='news-date'>Oct 27th, 2019</p>
+        <p className='news-date'>Feb 23rd, 2020</p>
       </div >
+      <img className='news-img' src={nef} alt='Screenshot of Aftermath posing in front of the corpse of Nefarian' />
+      <p className='news-p'>
+        The Lord of Blackrock Spire and son of Deathwing has been defeated. Epic cheers arose as his still body lay smoldering at our feet. &lt;Insert more RP text here&gt;
+      </p>
+      <p className='news-p'>
+        Great job everyone! Lot's of hard work put in with many consumes used! Got some great drops these first two weeks and looking forward to many more before Zul'Gurub!
+      </p>
+      <p className='news-p'>
+        We are looking for a few more good players to join our ranks (shaman and warlocks in particular.) Join our discord to find out more!
+      </p>
+      <div className='empty'></div>
+      <p className='news-date'>Oct 27th, 2019</p>
       <img className='news-img' src={rag} alt='Screenshot of Aftermath posing in front of the corpse of Ragnaros' />
       <p className='news-p'>We started the week off right with a little PvP on the alliance boat in Theramore. When Scooty started yelling at us, we knew it was time to put on our game faces. Onyxia didn't stand a chance. One-shot. On to MC...</p>
       <p className='news-p'>Luci, Mag, Gehennas, Garr, Shazz, Baron, Golemagg, Sulfuron, Domo... all one-shots. It was late so we decided to tackle our first Rag kill on Sunday.</p>
@@ -34,7 +32,7 @@ const Home = () => {
       <p className='news-p'>Feeling pretty confident of ourselves we go back to Onyxia to kill her bitch ass one more time this week. You guessed it... one-shot.</p>
       <p className='news-p'>I couldn't be more proud of the team we have built here. Thank you all so much for being amazing! Now time to farm them lootz and get ready for PvP in phase 2!</p>
       <div className='empty'></div>
-    </div>
+    </div >
   )
 };
 
