910d2a12164af3e821560909f011d12ee4f776da
Chad Pjontek <chadpjontek@gmail.com>
2021-07-02 01:29:06 -0400

    chore: disable emoji linting

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react';
 import logo from '../images/aftermath-min.jpg';
 import rag from '../images/aftermath-one-shot-rag-min.jpg'
